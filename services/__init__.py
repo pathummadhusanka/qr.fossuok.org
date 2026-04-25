6b76b650e2aef71ba5206209b1a0171997c6fddb
DasunNethsara-04 <techsaralk.pro@gmail.com>
2026-04-25 08:49:33 +0530

    chore: export new registration service functions

--- services/__init__.py
+++ services/__init__.py
@@ -6,8 +6,12 @@ from .auth import (
     create_session_cookie,
     decode_session_cookie,
 )
-from .event import get_active_event, get_event_by_id
+from .user import (
+    auto_register_user, verify_user, get_qr_image, generate_qr_data_url,
+    get_user_profile, complete_user_profile,
+)
 from .mail import send_qr_email
+from .event import get_active_event, get_event_by_id
 from .registration import (
     get_user_registrations,
     get_available_events_for_user,
@@ -15,16 +19,14 @@ from .registration import (
     verify_registration,
     get_registration_qr_payload,
 )
-from .user import (
-    auto_register_user, verify_user, get_qr_image, generate_qr_data_url,
-    get_user_profile, complete_user_profile,
-)
 
 __all__ = [
     "auto_register_user",
     "verify_user",
     "get_qr_image",
     "generate_qr_data_url",
+    "get_user_profile",
+    "complete_user_profile",
     "send_qr_email",
     "build_github_redirect_url",
     "handle_supabase_callback",
@@ -34,4 +36,10 @@ __all__ = [
     "decode_session_cookie",
     "get_active_event",
     "get_event_by_id",
+    "get_user_registrations",
+    "get_available_events_for_user",
+    "register_for_event",
+    "verify_registration",
+    "get_registration_qr_payload",
 ]
+
